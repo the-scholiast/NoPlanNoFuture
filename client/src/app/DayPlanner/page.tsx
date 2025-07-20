647fda6342b5a11e0d89f0c8590ce5bcb9dacbe3
Tracy Chung <chuiyingchungccy@gmail.com>
2025-07-20 00:37:48 -0700

    Create DayPlanner page

---
+++ client/src/app/DayPlanner/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RadialPlanner from '../../components/RadialPlanner/RadialPlanner';
+
+function App() {
+    return (
+        <div className="App">
+            <RadialPlanner />
+        </div>
+    );
+}
+
+export default App;
