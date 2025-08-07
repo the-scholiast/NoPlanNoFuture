3032dac4e18c5eefe30427ee23489395c79ae67c
Woojin Song <wsong31@my.bcit.ca>
2025-08-07 16:48:59 -0700

    Fix day order to match getDay

--- client/src/lib/utils/constants.ts
+++ client/src/lib/utils/constants.ts
@@ -1,21 +1,21 @@
 export const DAYS_OF_WEEK = [
+  'sunday', 
   'monday', 
   'tuesday', 
   'wednesday', 
   'thursday', 
   'friday', 
   'saturday',
-  'sunday', 
 ] as const; // Create literal types of a tuple instead of string[]
 
 export type DayOfWeek = typeof DAYS_OF_WEEK[number]; // Get the type of any element in this array (the date names)
 
 export const DAY_ABBREVIATIONS: Record<DayOfWeek, string> = {
+  sunday: 'Sun',
   monday: 'Mon',
   tuesday: 'Tue',
   wednesday: 'Wed',
   thursday: 'Thu',
   friday: 'Fri',
   saturday: 'Sat',
-  sunday: 'Sun',
 };
