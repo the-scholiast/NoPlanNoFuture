e8656be280cd934efcc75e1642ee29c7414cd291
Woojin Song <wsong31@my.bcit.ca>
2025-08-09 12:23:17 -0700

    Add toggle view component for daily section

---
+++ client/src/components/todo/DailyTaskToggle.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Calendar, CalendarDays } from 'lucide-react';
+
+interface DailyTaskToggleProps {
+  showAllTasks: boolean;
+  onToggle: (showAll: boolean) => void;
+  className?: string;
+}
+
+export const DailyTaskToggle: React.FC<DailyTaskToggleProps> = ({
+  showAllTasks,
+  onToggle,
+  className = ""
+}) => {
+  return (
+    <div className={`flex border rounded-md ${className}`}>
+      <Button
+        variant={!showAllTasks ? "default" : "ghost"}
+        size="sm"
+        onClick={() => onToggle(false)}
+        className="rounded-r-none border-r h-8 px-2"
+      >
+        <Calendar className="h-3 w-3 mr-1" />
+        Today Only
+      </Button>
+      <Button
+        variant={showAllTasks ? "default" : "ghost"}
+        size="sm"
+        onClick={() => onToggle(true)}
+        className="rounded-l-none h-8 px-2"
+      >
+        <CalendarDays className="h-3 w-3 mr-1" />
+        Show All
+      </Button>
+    </div>
+  );
+};
+
+export default DailyTaskToggle;
