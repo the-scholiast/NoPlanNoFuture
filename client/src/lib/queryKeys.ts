c77ff1d080066599b3d722f1843ad2c273f683d2
Woojin Song <wsong31@my.bcit.ca>
2025-08-16 14:11:17 -0700

    Decompose query keys

---
+++ client/src/lib/queryKeys.ts
@@ -0,0 +1,12 @@
+// Contains the keys of all query keys for caches
+export const todoKeys = {
+  all: ['todos'] as const,
+  today: ['recurring-todos', 'today'] as const,
+  upcoming: ['recurring-todos', 'upcoming'] as const,
+  completed: ['completed-tasks'] as const,
+  timetable: {
+    tasks: ['timetable-tasks'] as const,
+    week: (weekStartDate: string) => ['timetable-week', weekStartDate] as const,
+    allWeeks: ['timetable-week'] as const,
+  }
+}
