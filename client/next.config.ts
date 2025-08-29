669eb8199a04f2bfebefb529fbdee32485c17cd8
Woojin Song <wsong31@my.bcit.ca>
2025-08-29 14:25:55 -0700

    Re-enable eslint and typscript checks during build

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Disable ESLint during builds
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Disable TypeScript errors during builds  
-    ignoreBuildErrors: true,
-  },
+  // eslint: {
+  //   // Disable ESLint during builds
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   // Disable TypeScript errors during builds  
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
