59c46dece24c79a91ed7a3e7f124724b4662d8ae
Woojin Song <wsong31@my.bcit.ca>
2025-09-01 21:33:03 -0700

    Remove taskValidation.js

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -6,8 +6,6 @@ export * from './todoController.js'
 export * from './exerciseController.js'
 export * from './workoutController.js'
 
-export * from './taskValidation.js'
-
 export * from './profileController.js'
 export * from './statsGymController.js'
 export * from './statsTodoController.js'
